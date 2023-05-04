ad3f44c37f04d963923b9eb25c7f0edc28cb9d17
fotini <f.batzoni@gmail.com>
2023-05-04 15:49:50 +0300

    ContactForm View

---
+++ CleanBlog.Core/ViewModels/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CleanBlog.Core.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="Pease enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Pease enter your email address")]
+        [EmailAddress(ErrorMessage = "You must enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Pease enter your message")]
+        [MaxLength(500, ErrorMessage="Your message should not be longer than 500 characters")]
+        public string Message { get; set; }
+    }
+}
